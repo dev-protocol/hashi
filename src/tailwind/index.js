51227eb00df01cbfbe07c0f27319bc4001e3ff1c
Matteu <matteugt@gmail.com>
2022-02-17 13:21:42 +0800

    UPDATE: Partialized tailwind config

--- src/tailwind/index.js
+++ src/tailwind/index.js
@@ -20,6 +20,8 @@
  *  SOFTWARE.
  */
 
+import { hsColorTokens } from "./_colors";
+
 module.exports = {
   theme: {
     screens: {
@@ -28,73 +30,7 @@ module.exports = {
       large: '890px'
     },
     colors: {
-      surface: {
-        200: 'var(--hs-theme-surface-200)',
-        300: 'var(--hs-theme-surface-300)',
-        400: 'var(--hs-theme-surface-400)',
-        600: 'var(--hs-theme-surface-600)',
-        'ink': 'var(--hs-theme-surface-ink)'
-      },
-      neutral: {
-        200: 'var(--hs-theme-neutral-200)',
-        300: 'var(--hs-theme-neutral-300)',
-        400: 'var(--hs-theme-neutral-400)',
-        600: 'var(--hs-theme-neutral-600)',
-        'ink': 'var(--hs-theme-neutral-ink)'
-      },
-      disabled: {
-        DEFAULT: 'var(--hs-theme-disabled)',
-        'ink': 'var(--hs-theme-disabled-ink)'
-      },
-      scarlet: {
-        200: 'var(--hs-theme-scarlet-200)',
-        300: 'var(--hs-theme-scarlet-300)',
-        400: 'var(--hs-theme-scarlet-400)',
-        600: 'var(--hs-theme-scarlet-600)',
-        'ink': 'var(--hs-theme-scarlet-ink)'
-      },
-      plox: {
-        200: 'var(--hs-theme-plox-200)',
-        300: 'var(--hs-theme-plox-300)',
-        400: 'var(--hs-theme-plox-400)',
-        600: 'var(--hs-theme-plox-600)',
-        'ink': 'var(--hs-theme-plox-ink)'
-      },
-      'native-blue': {
-        200: 'var(--hs-theme-native-blue-200)',
-        300: 'var(--hs-theme-native-blue-300)',
-        400: 'var(--hs-theme-native-blue-400)',
-        600: 'var(--hs-theme-native-blue-600)',
-        'ink': 'var(--hs-theme-native-blue-ink)'
-      },
-      'bright-cyan': {
-        200: 'var(--hs-theme-bright-cyan-200)',
-        300: 'var(--hs-theme-bright-cyan-300)',
-        400: 'var(--hs-theme-bright-cyan-400)',
-        600: 'var(--hs-theme-bright-cyan-600)',
-        'ink': 'var(--hs-theme-bright-cyan-ink)'
-      },
-      success: {
-        200: 'var(--hs-theme-success-200)',
-        300: 'var(--hs-theme-success-300)',
-        400: 'var(--hs-theme-success-400)',
-        600: 'var(--hs-theme-success-600)',
-        'ink': 'var(--hs-theme-success-ink)'
-      },
-      warning: {
-        200: 'var(--hs-theme-warning-200)',
-        300: 'var(--hs-theme-warning-300)',
-        400: 'var(--hs-theme-warning-400)',
-        600: 'var(--hs-theme-warning-600)',
-        'ink': 'var(--hs-theme-warning-ink)'
-      },
-      danger: {
-        200: 'var(--hs-theme-danger-200)',
-        300: 'var(--hs-theme-danger-300)',
-        400: 'var(--hs-theme-danger-400)',
-        600: 'var(--hs-theme-danger-600)',
-        'ink': 'var(--hs-theme-danger-ink)'
-      },
+      hsColorTokens
     },
     // rounded-[]
     borderRadius: {
