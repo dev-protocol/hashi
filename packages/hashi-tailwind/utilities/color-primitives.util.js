7c8847d2f1eb6ad0cc6ac72193c60340ffd27133
Matteu <matteugt@gmail.com>
2023-05-04 20:07:37 +0800

    UPDATE(tw): Update color tokens

--- packages/hashi-tailwind/utilities/color-primitives.util.js
+++ packages/hashi-tailwind/utilities/color-primitives.util.js
@@ -1,5 +1,6 @@
 /*
- *  Copyright (c) 2022 Dev Protocol
+ *
+ *  Copyright (c) 2023 Dev Protocol
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -18,85 +19,107 @@
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *  SOFTWARE.
+ *
  */
 
 module.exports = {
-  'dp-white': {
-    200: '#ffffff',
-    300: '#f1f1f1',
-    400: '#d7d7d7',
-    600: '#c4c4c4',
-    'ink': '#040b10',
-  },
-  'dp-black': {
-    200: '#383838',
-    300: '#272727',
-    400: '#171717',
-    600: '#000000',
-    'ink': '#ffffff',
-  },
-  'dp-blue-grey': {
-    200: '#3a4158',
-    300: '#222b3d',
-    400: '#111827',
-    600: '#040b10',
-    ink: '#ffffff',
-  },
   'dp-black-alpha': {
-    DEFAULT: 'rgba(0, 0, 0, .12)',
+    'default': 'rgba(0, 0, 0, .12)',
     'ink': 'rgba(0, 0, 0, .37)',
   },
   'dp-white-alpha': {
-    DEFAULT: 'rgba(255, 255, 255, .12)',
+    'default': 'rgba(255, 255, 255, .12)',
     'ink': 'rgba(255, 255, 255, .37)',
   },
+  'dp-white': {
+    '200': '#ffffff',
+    '300': '#f1f1f1',
+    '400': '#d7d7d7',
+    '600': '#c4c4c4',
+    'ink': '#040b10',
+  },
+  'dp-black': {
+    '200': '#383838',
+    '300': '#272727',
+    '400': '#171717',
+    '600': '#000000',
+    'ink': '#ffffff',
+  },
+  'dp-red': {
+    '200': '#e45d5d',
+    '300': '#df3737',
+    '400': '#c92020',
+    '600': '#920d0d',
+    'ink': '#ffffff'
+  },
   'scarlet': {
-    200: '#ff7860',
-    300: '#ff583a',
-    400: '#ff3815',
-    600: '#b71f04',
+    '200': '#ff7860',
+    '300': '#ff583a',
+    '400': '#ff3815',
+    '600': '#b71f04',
+    'ink': '#ffffff',
+  },
+  'dp-orange': {
+    '200': '#ffa040',
+    '300': '#fd8835',
+    '400': '#ff6a00',
+    '600': '#c4570c',
+    'ink': '#ffffff'
+  },
+  'dp-yellow': {
+    '200': '#ffc751',
+    '300': '#ffbb27',
+    '400': '#fdad00',
+    '600': '#ac7600',
+    'ink': '#000000'
+  },
+  'dp-green': {
+    '200': '#67cf72',
+    '300': '#43c451',
+    '400': '#30a83d',
+    '600': '#197a23',
+    'ink': '#ffffff'
+  },
+  'bright-cyan': {
+    '200': '#51e0ff',
+    '300': '#27d9ff',
+    '400': '#00d0fd',
+    '600': '#008dac',
+    'ink': '#000000',
+  },
+  'native-blue': {
+    '200': '#88aeff',
+    '300': '#6d9bff',
+    '400': '#5b8bf5',
+    '600': '#315ab4',
+    'ink': '#ffffff',
+  },
+  'dp-blue-grey': {
+    '200': '#3a4158',
+    '300': '#222b3d',
+    '400': '#111827',
+    '600': '#040b10',
+    'ink': '#ffffff',
+  },
+  'hs-purple': {
+    '200': '#c0a2ee',
+    '300': '#a173e0',
+    '400': '#6c30bd',
+    '600': '#3b1570',
     'ink': '#ffffff',
   },
   'plox': {
-    200: '#eb48f8',
-    300: '#e71cf7',
-    400: '#d500e6',
-    600: '#91009c',
+    '200': '#eb48f8',
+    '300': '#e71cf7',
+    '400': '#d500e6',
+    '600': '#91009c',
     'ink': '#ffffff',
   },
-  'native-blue': {
-    200: '#88aeff',
-    300: '#6d9bff',
-    400: '#5b8bf5',
-    600: '#315ab4',
-    'ink': '#ffffff',
-  },
-  'bright-cyan': {
-    200: '#51e0ff',
-    300: '#27d9ff',
-    400: '#00d0fd',
-    600: '#008dac',
-    'ink': '#000000',
-  },
-  'success': {
-    200: '#67cf72',
-    300: '#43c451',
-    400: '#30a83d',
-    600: '#197a23',
-    'ink': '#ffffff',
-  },
-  'warning': {
-    200: '#ffc751',
-    300: '#ffbb27',
-    400: '#fdad00',
-    600: '#ac7600',
-    'ink': '#000000',
-  },
-  'danger': {
-    200: '#e45d5d',
-    300: '#df3737',
-    400: '#c92020',
-    600: '#920d0d',
-    'ink': '#ffffff',
-  },
+  'hana': {
+    '200': '#ffb8d7',
+    '300': '#ff77a9',
+    '400': '#ec407a',
+    '600': '#b4004e',
+    'ink': '#ffffff'
+  }
 };
