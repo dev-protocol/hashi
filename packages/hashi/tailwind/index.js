5503b5f2961184290361b798f1e63cc5157dfb70
Matteu <matteugt@gmail.com>
2023-07-21 21:30:07 +0800

    FEAT: Add extension file to tailwind integration

--- packages/hashi/tailwind/index.js
+++ packages/hashi/tailwind/index.js
@@ -1,5 +1,4 @@
 /*
- *
  *  Copyright (c) 2023 Dev Protocol
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -19,7 +18,6 @@
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *  SOFTWARE.
- *
  */
 
 const hsColorTokens = {
