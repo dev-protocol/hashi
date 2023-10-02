9dfcec2bc566122cd629c504503a37630f533dfa
Matteu <matteugt@gmail.com>
2023-10-02 13:29:13 +0800

    UPDATE: Update page components

--- docs/src/components/HSCard.tsx
+++ docs/src/components/HSCard.tsx
@@ -1,54 +1,56 @@
-/*
- *  Copyright (c) 2022 Dev Protocol
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- */
+///
+//  Copyright (c) 2023 Dev Protocol
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+///
 
 import React from 'react';
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 interface HSCardProps {
     title: string;
     description?: string;
-    actions?: React.ReactElement | string;
+    media?: string;
+    mediaAlt?: string;
+    children?: React.ReactNode;
 }
 
-const HSCard: React.FC<HSCardProps> = ({ title, description, actions, children }) => {
+const HSCard: React.FC<HSCardProps> = ({ title, description, media, mediaAlt, children }) => {
     return (
-        <div className="hs-card is-filled">
-            <div className="hs-card__header">
+        <div className="hs-card is-filled is-raised">
+            <div>
+              <section className="hs-card__media">
+                <img src={useBaseUrl(`/img/${media}`)} alt={mediaAlt}/>
+              </section>
+              <header className="hs-card__header">
                 <div className="hs-card__mast">
                   <h2 className="hs-card__title">{ title }</h2>
                   { description &&
                     <p className="hs-card__subtitle">{ description }</p>
                   }
                 </div>
+              </header>
+              <div/>
             </div>
-            { children &&
-            <div className="hs-card__content">
-                { children }
-            </div>
-            }
-            { actions &&
-            <div className="hs-card__actions">
-                { actions }
-            </div>
-            }
+            <footer className="hs-card__footer">
+              { children }
+            </footer>
         </div>
     );
 };
